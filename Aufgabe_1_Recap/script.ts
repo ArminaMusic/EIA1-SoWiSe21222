de8c8f865925f30abeb10849c5fa80e14ca26905
Armina Music <armina.music@hs-furtwangen.de>
2021-10-13 12:05:00 +0200

    Recap_Ts_upload_needs work

---
+++ Aufgabe_1_Recap/script.ts
@@ -0,0 +1,29 @@
+const subject: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+const prediacte: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", " zerstört"];
+const object: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+//console.log(subject, prediacte, object);
+
+for (let i = 6; i >= 1; i--) {
+//console.log(i);
+console.log(getVerse(subject, prediacte, object));
+
+}
+
+function getVerse(_subject: string[], _predicate: string[], _object: string[]): string {
+    let vers: string = "";
+
+    let randomS: number = Math.floor(Math.random() * _subject.length);
+    let splicedS: string = _subject.splice(randomS, 1)[0];
+    vers = vers + " " + splicedS;
+
+    let randomP: number = Math.floor(Math.random() * _predicate.length);
+    let splicedP: string = _predicate.splice(randomP, 1)[0];
+    vers = vers + " " + splicedP;
+
+    let randomO: number = Math.floor(Math.random() * _object.length);
+    let splicedO: string = _object.splice(randomO, 1)[0];
+    vers = vers + " " + splicedO;
+
+    return vers;
+}
