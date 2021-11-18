250f5cf4c0fa6bd16ab7db31bad90cf1c61e3fd3
Armina Music <armina.music@hs-furtwangen.de>
2021-11-18 15:58:16 +0100

    L08 erstellt

---
+++ Augabe_8/script.js
@@ -0,0 +1,8 @@
+/*
+Aufgabe: L08.1_GenerativeKunst
+Name: Armina Music
+Matrikel: 268021
+Datum: 18.11.2021
+Kommentar: es6 Version
+*/
+//# sourceMappingURL=script.js.map
---
+++ Augabe_8/script.ts
@@ -0,0 +1,12 @@
+/*
+Aufgabe: L08.1_GenerativeKunst
+Name: Armina Music
+Matrikel: 268021
+Datum: 18.11.2021
+Kommentar: es6 Version
+*/
+
+namespace L08_1_GenerativeKunst {
+
+}
+
