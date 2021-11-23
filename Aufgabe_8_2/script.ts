d6ca1710bd51620ae7382ae0a109b71d667fcbdc
Armina Music <armina.music@hs-furtwangen.de>
2021-11-23 13:58:54 +0100

    Aufgabe 8_2 erstellt

---
+++ Aufgabe_8_2/script.ts
@@ -0,0 +1,13 @@
+/*
+Aufgabe: L08.2_GoldenerHerbst
+Name: Armina Music
+Matrikel: 268021
+Datum: 23.11.2021
+Kommentar: es6 Version
+*/
+
+namespace L08_1_GenerativeKunst {
+    interface Vector {
+        x: number;
+        y: number;
+    }
