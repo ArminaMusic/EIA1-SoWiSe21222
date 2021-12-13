17226c280ae8c9f9b73fab2e82fe08d43613de2d
Armina Music <armina.music@hs-furtwangen.de>
2021-12-13 14:45:50 +0100

    💨 TS erstellt

---
+++ Aufgabe_9_2/Leaf.ts
@@ -0,0 +1,24 @@
+/*
+Aufgabe: L09.2_GoldenerHerbstClasses
+Name: Armina Music
+Matrikel: 268021
+Datum: 13.12.2021
+Kommentar: es6 Version
+*/
+
+namespace Golden {
+    export class Leaf {
+
+        constructor() {
+            //
+        }
+
+        fall(): void {
+            //
+        }
+
+        draw(): void {
+            //
+        }
+    }
+}
---
+++ Aufgabe_9_2/Path.ts
@@ -0,0 +1,31 @@
+/*
+Aufgabe: L09.2_GoldenerHerbstClasses
+Name: Armina Music
+Matrikel: 268021
+Datum: 13.12.2021
+Kommentar: es6 Version
+*/
+
+namespace Golden {
+    export function drawLeaf(): void {
+        //
+    }
+
+    function Leaf(): void {
+        //
+    }
+
+    function Leaf(): void {
+        //
+    }
+
+    function Leaf(): void {
+        //
+    }
+
+
+    function drawSquirrel(): void {
+
+        //
+    }
+}
